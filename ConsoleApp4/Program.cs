868c5cf1164c3f5ead9242538300d2771acd79ab
Денис Ищук <denis.ishuk@gmail.com>
2022-02-21 14:25:20 +0300

    ыйвафывафыва

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Работать с git - ВАЖНО!!");
-
+Console.WriteLine("Это важный навык программиста!");
